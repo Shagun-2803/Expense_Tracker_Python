288f809284e3df96b5b4d118f779748c0adadb24
Shagun-2803 <contactshagunchaudhari@gmail.com>
2026-05-22 10:54:13 +0530

    View Expense Function added

--- python.py
+++ python.py
@@ -1,30 +1,48 @@
+expenses = []
 
 def add_expense(expenses):
   new_expense = {}
+
   title = input("Enter title of expense: ")
   new_expense["title"]  = title
+
   category = input("Enter the category: ").lower()
   new_expense["category"] = category
+
   amount = int(input("Enter amount: "))
   new_expense["amount"] = amount
+
   expenses.append(new_expense)
-  print("Expence added successfully")
 
+  print("Expense added successfully")
 
+def view_expenses(expenses):
+  if(len(expenses)==0):
+    print("No expenses added yet!")
+    return
 
-expenses = []
-
-
+  for expense in expenses :
+    print(f"Title: {expense['title']}")
+    print(f"Category: {expense['category']}")
+    print(f"Amount: {expense['amount']}")
+    print()
 
 
 while(True):
   print("\n1. Add Expense")
   print("2. View Expense")
   print("3. Exit")
+
   choice = int(input("Enter your choice: "))
+
   if(choice == 1):
     add_expense(expenses)
-  if(choice == 3):
+  
+  elif(choice == 2):
+    view_expenses(expenses)
+  
+  elif(choice == 3):
     break
-  if(choice > 3 or choice <= 0):
+  
+  else:
     print("Invalid Choice")
